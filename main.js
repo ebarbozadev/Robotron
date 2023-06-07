cec5ac354ecb0d306368dccc117c2183402b166b
E.Barboza <e.barboza@edu.unipar.br>
2023-06-07 17:09:28 -0300

    Finalizando projeto - Curso não cumpriu com o que prometeu

--- main.js
+++ main.js
@@ -1,10 +1,59 @@
-let botaoBracos = document.getElementsByClassName('controle-contador')[0].value;
-console.log(botaoBracos)
+const controle = document.querySelectorAll("[data-controle]");
+const estatistica = document.querySelectorAll("[data-estatistica]");
 
-const botaoBracosAdicionar = document.getElementsByClassName('controle-ajuste')[1];
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos": {
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas": {
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes": {
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+};
 
-// function somarBracos(){
-//     botaoBracos = botaoBracos + '1';
-// }
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode);
+        atualizaEstatistica(evento.target.dataset.peca);
+    });
+});
 
-botaoBracosAdicionar.addEventListener("click", () => {console.log(botaoBracos)})
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector("[data-contador]");
+
+    if (operacao === "-") {
+        peca.value = parseInt(peca.value) - 1;
+    } else {
+        peca.value = parseInt(peca.value) + 1;
+    }
+}
+
+function atualizaEstatistica(peca) {
+    estatistica.forEach((elemento) => {
+        elemento.textContent =
+            parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica];
+    });
+}
